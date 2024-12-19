1cddc5609986b1d5660aa0f8f2c4953eea8f3503
Dat Le <dat.le@qbrick.com>
2024-12-19 21:38:23 +0100

    Set up base structure

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          &copy; Copyright by{" "}
+          <a href="datle.com" target="_blank">
+            Datle.com
+          </a>
+          . Intended for learning or your porlolio haha
+        </p>
+        <p>
+          <span className="u-bold u-italic">Not allowed</span> to use as your
+          own teaching material
+        </p>
+      </small>
+    </footer>
+  );
+}
