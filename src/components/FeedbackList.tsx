9a1ebdc93ae82be276b10d048c71295bf977df7d
Dat Le <dat.le@qbrick.com>
2024-12-20 15:15:16 +0100

    Fetch data from api

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,20 +1,47 @@
+import { useEffect, useState } from "react";
 import FeedbackItemComponent from "./FeedbackItemComponent";
+import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
-  const feedbackItem = {
-    upvoteCount: 0,
-    badgeLetter: "A",
-    companyName: "Company",
-    text: "This is a feedback item",
-    daysAgo: 1,
-  };
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessages, setErrorMessages] = useState("");
 
-  const feedbackItems = [feedbackItem];
+  useEffect(() => {
+    const fetchFeedbacksItems = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+        );
+
+        if (!response.ok) {
+          throw new Error();
+        }
+
+        const data = await response.json();
+        setFeedbackItems(data.feedbacks);
+      } catch (error) {
+        setErrorMessages("An error occurred while fetching feedbacks.");
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchFeedbacksItems();
+  }, []);
 
   return (
     <ol className="feedback-list">
+      {isLoading && <Spinner />}
+      {errorMessages && <ErrorMessage message={errorMessages} />}
       {feedbackItems.map((feedbackItem) => (
-        <FeedbackItemComponent feedbackItem={feedbackItem} />
+        <FeedbackItemComponent
+          key={feedbackItem.id}
+          feedbackItem={feedbackItem}
+        />
       ))}
     </ol>
   );
