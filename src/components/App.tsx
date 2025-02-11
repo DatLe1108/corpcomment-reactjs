00554534e9a932d7c831be1ef6c1824493ee3fc7
Dat Le <dat.le@qbrick.com>
2025-02-11 08:49:14 +0100

    remove later

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
-import HashtagList from "./hashtag/HashtagList";
-import Footer from "./layout/Footer";
-import Container from "./layout/Container";
+// import HashtagList from "./hashtag/HashtagList";
+// import Footer from "./layout/Footer";
+// import Container from "./layout/Container";
 import { useEffect } from "react";
 import { useFeedbackItemsStore } from "./stores/feedbackItemsStore";
 
@@ -15,11 +15,22 @@ function App() {
 
   return (
     <div className="app">
-      <Footer />
+      {/* <Footer />
 
       <Container />
 
-      <HashtagList />
+      <HashtagList /> */}
+      <iframe
+        data-qplayer-media-id="40ca68aa-3e09-4d57-a9dc-0b03928585f6"
+        title="Jumo Jump"
+        data-cookieblock-src="https://play2.qbrick.com/qplayer-beta/index.html?accountId=123516&mediaId=40ca68aa-3e09-4d57-a9dc-0b03928585f6&configId=Interactive"
+        data-cookieconsent="statistics"
+        allowFullScreen={true}
+        frameBorder={0}
+        // border="0"
+        height="360"
+        width="640"
+      />
     </div>
   );
 }
